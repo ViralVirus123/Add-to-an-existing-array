abece28f59d0dedb0af32457854ddf569038fad2
ViralVirus123 <alexander.hammond-garcia@patelhs.org>
2020-12-09 05:19:25 +0000

    First code attempt

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    String [] rosterOne = new String[10];
+
+  Scanner input = new Scanner(System.in);
+  String[] BarrysRoster = new String[10];
+
+  for (int i = 0; i < rosterOne.length; i++)
+  {
+    rosterOne[i] = input.next();
+  }
+    String [] rosterTwo = new Sting[12];
+    for (int index = 0; index < roster1.length; index++)
+  {
+    roster1[index] = roster2[index];
+    System.out.println(roster1[index]);
   }
 }
